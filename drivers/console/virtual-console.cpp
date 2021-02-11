bde83d658074ae8417db1b6b133ff981f4c8ed60
Tom Spink <tspink@gmail.com>
2021-02-11 16:11:12 +0000

    Support directly writing to the virtual console framebuffer via pwrite

--- drivers/console/virtual-console.cpp
+++ drivers/console/virtual-console.cpp
@@ -17,6 +17,7 @@ using namespace infos::drivers;
 using namespace infos::drivers::console;
 using namespace infos::drivers::input;
 using namespace infos::util;
+using namespace infos::fs;
 
 const DeviceClass VirtualConsole::VirtualConsoleDeviceClass(Console::ConsoleDeviceClass, "vc");
 
@@ -24,7 +25,8 @@ VirtualConsole::VirtualConsole() : _current_mod_mask(None), _current_pos(0), _bu
 {
 	_buffer = new uint16_t[_width * _height];
 
-	for (int i = 0; i < _width * _height; i++) {
+	for (int i = 0; i < _width * _height; i++)
+	{
 		_buffer[i] = 0x0700;
 	}
 }
@@ -34,7 +36,7 @@ VirtualConsole::~VirtualConsole()
 	delete _buffer;
 }
 
-void VirtualConsole::attach_terminal(terminal::Terminal* terminal)
+void VirtualConsole::attach_terminal(terminal::Terminal *terminal)
 {
 	_terminal = terminal;
 	_terminal->attach_output(*this);
@@ -42,7 +44,8 @@ void VirtualConsole::attach_terminal(terminal::Terminal* terminal)
 
 void VirtualConsole::key_up(Keys::Keys key)
 {
-	switch (key) {
+	switch (key)
+	{
 	case Keys::KEY_LSHIFT:
 	case Keys::KEY_RSHIFT:
 		_current_mod_mask = _current_mod_mask & ~Shift;
@@ -55,53 +58,60 @@ void VirtualConsole::key_up(Keys::Keys key)
 
 void VirtualConsole::key_down(Keys::Keys key)
 {
-#define KEY_MAPPING(_kc, _upper, _lower) case Keys::_kc: if (_current_mod_mask & Shift) ch = _upper; else ch = _lower; break
+#define KEY_MAPPING(_kc, _upper, _lower) \
+	case Keys::_kc:                      \
+		if (_current_mod_mask & Shift)   \
+			ch = _upper;                 \
+		else                             \
+			ch = _lower;                 \
+		break
 	char ch = 0;
-	switch (key) {
-	KEY_MAPPING(KEY_A, 'A', 'a');
-	KEY_MAPPING(KEY_B, 'B', 'b');
-	KEY_MAPPING(KEY_C, 'C', 'c');
-	KEY_MAPPING(KEY_D, 'D', 'd');
-	KEY_MAPPING(KEY_E, 'E', 'e');
-	KEY_MAPPING(KEY_F, 'F', 'f');
-	KEY_MAPPING(KEY_G, 'G', 'g');
-	KEY_MAPPING(KEY_H, 'H', 'h');
-	KEY_MAPPING(KEY_I, 'I', 'i');
-	KEY_MAPPING(KEY_J, 'J', 'j');
-	KEY_MAPPING(KEY_K, 'K', 'k');
-	KEY_MAPPING(KEY_L, 'L', 'l');
-	KEY_MAPPING(KEY_M, 'M', 'm');
-	KEY_MAPPING(KEY_N, 'N', 'n');
-	KEY_MAPPING(KEY_O, 'O', 'o');
-	KEY_MAPPING(KEY_P, 'P', 'p');
-	KEY_MAPPING(KEY_Q, 'Q', 'q');
-	KEY_MAPPING(KEY_R, 'R', 'r');
-	KEY_MAPPING(KEY_S, 'S', 's');
-	KEY_MAPPING(KEY_T, 'T', 't');
-	KEY_MAPPING(KEY_U, 'U', 'u');
-	KEY_MAPPING(KEY_V, 'V', 'v');
-	KEY_MAPPING(KEY_W, 'W', 'w');
-	KEY_MAPPING(KEY_X, 'X', 'x');
-	KEY_MAPPING(KEY_Y, 'Y', 'y');
-	KEY_MAPPING(KEY_Z, 'Z', 'z');
+	switch (key)
+	{
+		KEY_MAPPING(KEY_A, 'A', 'a');
+		KEY_MAPPING(KEY_B, 'B', 'b');
+		KEY_MAPPING(KEY_C, 'C', 'c');
+		KEY_MAPPING(KEY_D, 'D', 'd');
+		KEY_MAPPING(KEY_E, 'E', 'e');
+		KEY_MAPPING(KEY_F, 'F', 'f');
+		KEY_MAPPING(KEY_G, 'G', 'g');
+		KEY_MAPPING(KEY_H, 'H', 'h');
+		KEY_MAPPING(KEY_I, 'I', 'i');
+		KEY_MAPPING(KEY_J, 'J', 'j');
+		KEY_MAPPING(KEY_K, 'K', 'k');
+		KEY_MAPPING(KEY_L, 'L', 'l');
+		KEY_MAPPING(KEY_M, 'M', 'm');
+		KEY_MAPPING(KEY_N, 'N', 'n');
+		KEY_MAPPING(KEY_O, 'O', 'o');
+		KEY_MAPPING(KEY_P, 'P', 'p');
+		KEY_MAPPING(KEY_Q, 'Q', 'q');
+		KEY_MAPPING(KEY_R, 'R', 'r');
+		KEY_MAPPING(KEY_S, 'S', 's');
+		KEY_MAPPING(KEY_T, 'T', 't');
+		KEY_MAPPING(KEY_U, 'U', 'u');
+		KEY_MAPPING(KEY_V, 'V', 'v');
+		KEY_MAPPING(KEY_W, 'W', 'w');
+		KEY_MAPPING(KEY_X, 'X', 'x');
+		KEY_MAPPING(KEY_Y, 'Y', 'y');
+		KEY_MAPPING(KEY_Z, 'Z', 'z');
 
-	KEY_MAPPING(KEY_0, ')', '0');
-	KEY_MAPPING(KEY_1, '!', '1');
-	KEY_MAPPING(KEY_2, '"', '2');
-	KEY_MAPPING(KEY_3, '#', '3');
-	KEY_MAPPING(KEY_4, '$', '4');
-	KEY_MAPPING(KEY_5, '%', '5');
-	KEY_MAPPING(KEY_6, '^', '6');
-	KEY_MAPPING(KEY_7, '&', '7');
-	KEY_MAPPING(KEY_8, '*', '8');
-	KEY_MAPPING(KEY_9, '(', '9');
+		KEY_MAPPING(KEY_0, ')', '0');
+		KEY_MAPPING(KEY_1, '!', '1');
+		KEY_MAPPING(KEY_2, '"', '2');
+		KEY_MAPPING(KEY_3, '#', '3');
+		KEY_MAPPING(KEY_4, '$', '4');
+		KEY_MAPPING(KEY_5, '%', '5');
+		KEY_MAPPING(KEY_6, '^', '6');
+		KEY_MAPPING(KEY_7, '&', '7');
+		KEY_MAPPING(KEY_8, '*', '8');
+		KEY_MAPPING(KEY_9, '(', '9');
 
-	KEY_MAPPING(KEY_HYPHEN, '_', '-');
-	KEY_MAPPING(KEY_EQUALS, '+', '=');
-	KEY_MAPPING(KEY_BACKSLASH, '|', '\\');
-	KEY_MAPPING(KEY_FORSLASH, '?', '/');
+		KEY_MAPPING(KEY_HYPHEN, '_', '-');
+		KEY_MAPPING(KEY_EQUALS, '+', '=');
+		KEY_MAPPING(KEY_BACKSLASH, '|', '\\');
+		KEY_MAPPING(KEY_FORSLASH, '?', '/');
 
-	KEY_MAPPING(KEY_DOT, '>', '.');
+		KEY_MAPPING(KEY_DOT, '>', '.');
 
 	case Keys::KEY_SPACE:
 		ch = ' ';
@@ -127,42 +137,57 @@ void VirtualConsole::key_down(Keys::Keys key)
 	_terminal->append_to_read_buffer(ch);
 }
 
-int VirtualConsole::write(const void* buffer, size_t size)
+int VirtualConsole::write(const void *buffer, size_t size)
 {
 	if (!_buffer)
 		return 0;
 
 	int attr = 0x0700;
 	int state = 0;
-	for (unsigned int i = 0; i < size; i++) {
+	for (unsigned int i = 0; i < size; i++)
+	{
 		char c = ((uint8_t *)buffer)[i];
 
-		if (state == 1) {
+		if (state == 1)
+		{
 			attr = (int)c << 8;
 			state = 0;
-		} else if (state == 0) {
-			if (c == '\n') {
+		}
+		else if (state == 0)
+		{
+			if (c == '\n')
+			{
 				_current_pos += _width - (_current_pos % _width);
-			} else if (c == '\r') {
+			}
+			else if (c == '\r')
+			{
 				_current_pos -= (_current_pos % _width);
-			} else if (c == '\33') {
+			}
+			else if (c == '\33')
+			{
 				state = 1;
-			} else if (c == '\b') {
+			}
+			else if (c == '\b')
+			{
 				_current_pos--;
 				_buffer[_current_pos] = attr | ' ';
-			} else {
+			}
+			else
+			{
 				_buffer[_current_pos] = attr | c;
 				_current_pos++;
 			}
 
-			if (_current_pos >= _width * _height) {
+			if (_current_pos >= _width * _height)
+			{
 				scroll_one_line();
 				_current_pos = _width * (_height - 1);
 			}
 		}
 	}
 
-	if (_ucb) {
+	if (_ucb)
+	{
 		_ucb(*this);
 	}
 
@@ -173,7 +198,24 @@ void VirtualConsole::scroll_one_line()
 {
 	memcpy(_buffer, &_buffer[_width], _width * (_height - 1) * 2);
 
-	for (int x = 0; x < _width; x++) {
+	for (int x = 0; x < _width; x++)
+	{
 		_buffer[x + (_width * (_height - 1))] = 0x0700;
 	}
+}
+
+File *VirtualConsole::open_as_file()
+{
+	return new VirtualConsoleFile(*this);
+}
+
+int VirtualConsoleFile::pwrite(const void *buffer, size_t size, off_t offset)
+{
+	if (size != 2)
+	{
+		return 0;
+	}
+
+	vc_._buffer[offset] = *(uint16_t *)buffer;
+	return 2;
 }
--- include/infos/drivers/console/virtual-console.h
+++ include/infos/drivers/console/virtual-console.h
@@ -2,15 +2,16 @@
 
 /*
  * include/drivers/console/virtual-console.h
- * 
+ *
  * InfOS
  * Copyright (C) University of Edinburgh 2016.  All Rights Reserved.
- * 
+ *
  * Tom Spink <tspink@inf.ed.ac.uk>
  */
 #pragma once
 
 #include <infos/drivers/console/console.h>
+#include <infos/fs/file.h>
 
 namespace infos
 {
@@ -20,33 +21,48 @@ namespace infos
 		{
 			class Terminal;
 		}
-		
+
 		namespace console
 		{
-			class VirtualConsole : public Console
+			class VirtualConsole;
+			class VirtualConsoleFile : public fs::File
 			{
 			public:
+				VirtualConsoleFile(VirtualConsole& vc) : vc_(vc) { }
+				virtual int pwrite(const void *buffer, size_t size, off_t offset);
+
+			private:
+				VirtualConsole& vc_;
+			};
+
+			class VirtualConsole : public Console
+			{
+				friend class VirtualConsoleFile;
+
+			public:
 				typedef void (*UpdateCallbackFn)(VirtualConsole& vc);
-				
+
 				static const DeviceClass VirtualConsoleDeviceClass;
 				const DeviceClass& device_class() const override { return VirtualConsoleDeviceClass; }
 
 				VirtualConsole();
 				virtual ~VirtualConsole();
-				
+
 				void key_up(input::Keys::Keys key) override;
 				void key_down(input::Keys::Keys key) override;
-				
+
 				int write(const void *buffer, size_t size);
-				
+
 				void set_buffer(uint16_t *buffer, UpdateCallbackFn ucb) { _buffer = buffer; _ucb = ucb; }
 				uint16_t *get_buffer() const { return _buffer; }
 				uint16_t get_buffer_position() const { return _current_pos; }
-				
+
 				void attach_terminal(terminal::Terminal *terminal);
-				
+
 				bool supports_colour() const override { return true; }
-							
+
+				virtual fs::File *open_as_file() override;
+
 			private:
 				enum KeyboardModifiers
 				{
@@ -56,12 +72,12 @@ namespace infos
 					Alt = 4,
 					CapsLock = 8
 				};
-				
+
 				friend KeyboardModifiers operator|(const KeyboardModifiers& l, const KeyboardModifiers& r)
 				{
 					return (KeyboardModifiers)((uint32_t)l | (uint32_t)r);
 				}
-				
+
 				friend KeyboardModifiers operator&(const KeyboardModifiers& l, const KeyboardModifiers& r)
 				{
 					return (KeyboardModifiers)((uint32_t)l & (uint32_t)r);
@@ -71,18 +87,18 @@ namespace infos
 				{
 					return (KeyboardModifiers)(~(uint32_t)l);
 				}
-				
+
 				KeyboardModifiers _current_mod_mask;
-				
+
 				constexpr static int _width = 80;
 				constexpr static int _height = 25;
-				
+
 				uint16_t _current_pos;
-				
+
 				terminal::Terminal *_terminal;
 				uint16_t *_buffer;
 				UpdateCallbackFn _ucb;
-				
+
 				void scroll_one_line();
 			};
 		}
