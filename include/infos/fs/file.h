864ecab053f8f3ebf514197b5d116ffdb8682a89
Kai <nankeen@users.noreply.github.com>
2021-03-10 23:37:36 +0000

    Add mmap syscall
    Add mmap() range check
    
    Change MMAP_ANON to -1 to make it an exclusive flag.
    
    Add check on mmap syscall so that it checks if a range of pages are
    allocated instead of only a single page.
    
    Move stack start address to a higher location in order to make space for
    a potential heap.

--- include/infos/fs/file.h
+++ include/infos/fs/file.h
@@ -1,30 +1,34 @@
 /* SPDX-License-Identifier: MIT */
 #pragma once
 
+#include <infos/mm/vma.h>
+#include <infos/kernel/process.h>
 #include <infos/define.h>
 
 namespace infos
 {
-	namespace fs
-	{
-		class File
-		{
-		public:
-			enum SeekType
-			{
-				SeekAbsolute,
-				SeekRelative,
-			};
+    namespace fs
+    {
+        class File
+        {
+        public:
+            enum SeekType
+            {
+                SeekAbsolute,
+                SeekRelative,
+            };
 
-			virtual ~File() { }
+            virtual ~File() { }
 
-			virtual int read(void *buffer, size_t size) { return 0; }
-			virtual int pread(void *buffer, size_t size, off_t off) { return 0; }
-			virtual int write(const void *buffer, size_t size) { return 0; }
-			virtual int pwrite(const void *buffer, size_t size, off_t off) { return 0; }
-			virtual void seek(off_t offset, SeekType type) { }
+            virtual int read(void *buffer, size_t size) { return 0; }
+            virtual int pread(void *buffer, size_t size, off_t off) { return 0; }
+            virtual int write(const void *buffer, size_t size) { return 0; }
+            virtual int pwrite(const void *buffer, size_t size, off_t off) { return 0; }
+            virtual void seek(off_t offset, SeekType type) { }
 
-			virtual void close() { }
-		};
-	}
+            virtual virt_addr_t mmap(mm::VMA& vma, virt_addr_t addr, size_t len, off_t offset) { return -1; }
+
+            virtual void close() { }
+        };
+    }
 }
--- include/infos/kernel/syscall.h
+++ include/infos/kernel/syscall.h
@@ -4,54 +4,56 @@
 #include <infos/kernel/om.h>
 
 namespace infos {
-	namespace kernel {
+    namespace kernel {
 
-		class SyscallManager {
-		public:
-			static const int MAX_SYSCALLS = 256;
+        class SyscallManager {
+        public:
+            static const int MAX_SYSCALLS = 256;
 
-			typedef unsigned long (*syscallfn)(unsigned long arg0, unsigned long arg1, unsigned long arg2, unsigned long arg3, unsigned long arg4, unsigned long arg5);
+            typedef unsigned long (*syscallfn)(unsigned long arg0, unsigned long arg1, unsigned long arg2, unsigned long arg3, unsigned long arg4, unsigned long arg5);
 
-			SyscallManager();
+            SyscallManager();
 
-			void RegisterSyscall(int nr, syscallfn fn);
-			unsigned long InvokeSyscall(int nr, unsigned long arg0, unsigned long arg1, unsigned long arg2, unsigned long arg3, unsigned long arg4, unsigned long arg5);
+            void RegisterSyscall(int nr, syscallfn fn);
+            unsigned long InvokeSyscall(int nr, unsigned long arg0, unsigned long arg1, unsigned long arg2, unsigned long arg3, unsigned long arg4, unsigned long arg5);
 
-		private:
-			syscallfn syscall_table_[MAX_SYSCALLS];
-		};
+        private:
+            syscallfn syscall_table_[MAX_SYSCALLS];
+        };
 
-		class DefaultSyscalls {
-		public:
-			static void sys_nop();
-			static void sys_yield();
+        class DefaultSyscalls {
+        public:
+            static void sys_nop();
+            static void sys_yield();
 
-			static ObjectHandle sys_open(uintptr_t filename, uint32_t flags);
-			static unsigned int sys_close(ObjectHandle h);
+            static ObjectHandle sys_open(uintptr_t filename, uint32_t flags);
+            static unsigned int sys_close(ObjectHandle h);
 
-			static unsigned int sys_read(ObjectHandle h, uintptr_t buffer, size_t size);
-			static unsigned int sys_write(ObjectHandle h, uintptr_t buffer, size_t size);
-			static unsigned int sys_pread(ObjectHandle h, uintptr_t buffer, size_t size, off_t off);
-			static unsigned int sys_pwrite(ObjectHandle h, uintptr_t buffer, size_t size, off_t off);
+            static unsigned int sys_read(ObjectHandle h, uintptr_t buffer, size_t size);
+            static unsigned int sys_write(ObjectHandle h, uintptr_t buffer, size_t size);
+            static unsigned int sys_pread(ObjectHandle h, uintptr_t buffer, size_t size, off_t off);
+            static unsigned int sys_pwrite(ObjectHandle h, uintptr_t buffer, size_t size, off_t off);
 
-			static ObjectHandle sys_opendir(uintptr_t path, uint32_t flags);
-			static unsigned int sys_readdir(ObjectHandle h, uintptr_t buffer);
-			static unsigned int sys_closedir(ObjectHandle h);
+            static ObjectHandle sys_opendir(uintptr_t path, uint32_t flags);
+            static unsigned int sys_readdir(ObjectHandle h, uintptr_t buffer);
+            static unsigned int sys_closedir(ObjectHandle h);
 
-			static void sys_exit(unsigned int rc);
+            static void sys_exit(unsigned int rc);
 
-			static ObjectHandle sys_exec(uintptr_t program, uintptr_t args);
-			static unsigned int sys_wait_proc(ObjectHandle h);
+            static ObjectHandle sys_exec(uintptr_t program, uintptr_t args);
+            static unsigned int sys_wait_proc(ObjectHandle h);
 
-			static ObjectHandle sys_create_thread(uintptr_t entry_point, uintptr_t arg);
-			static unsigned int sys_stop_thread(ObjectHandle h);
-			static unsigned int sys_join_thread(ObjectHandle h);
-			static unsigned long sys_usleep(unsigned long us);
-			static unsigned int sys_get_tod(uintptr_t tpstruct);
-			static void sys_set_thread_name(ObjectHandle thr, uintptr_t name);
-			static unsigned long sys_get_ticks();
+            static ObjectHandle sys_create_thread(uintptr_t entry_point, uintptr_t arg);
+            static unsigned int sys_stop_thread(ObjectHandle h);
+            static unsigned int sys_join_thread(ObjectHandle h);
+            static unsigned long sys_usleep(unsigned long us);
+            static unsigned int sys_get_tod(uintptr_t tpstruct);
+            static void sys_set_thread_name(ObjectHandle thr, uintptr_t name);
+            static unsigned long sys_get_ticks();
 
-			static void RegisterDefaultSyscalls(SyscallManager& mgr);
-		};
-	}
+            static virt_addr_t sys_mmap(virt_addr_t addr, size_t len, int flags, ObjectHandle fd, off_t offset);
+
+            static void RegisterDefaultSyscalls(SyscallManager& mgr);
+        };
+    }
 }
